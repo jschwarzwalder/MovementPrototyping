a64f286f02a7d28f578e366408798a72e2b2751e
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2017-09-14 22:57:13 -0700

    Movement Prototype

---
+++ Assets/Fly.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fly : MonoBehaviour
+{
+
+    [SerializeField]
+    private float speed = 3.5f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    transform.position += Vector3.forward * speed * Time.deltaTime;
+	}
+}
